83b4be2d660117ce28804880b4ad0d8008c97e54
Seunghyun Yu <yshyun123@gmail.com>
2018-06-17 00:05:44 +0900

    Store 만들기.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import reducers from './reducers';
 import App from './containers/App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducers);
 
 ReactDOM.render(
   <App />,
